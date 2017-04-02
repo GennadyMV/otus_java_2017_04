1e34591f731b54ace6f775e04b7f4acb61d84f66
petrelevich <petrelevich@yandex.ru>
2017-04-02 19:30:22 +0300

    First version

---
+++ src/main/java/ru/petrelevich/mainpkg/MainClass.java
@@ -0,0 +1,10 @@
+package ru.petrelevich.mainpkg;
+
+/**
+ * Created by sergey on 02.04.17.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
