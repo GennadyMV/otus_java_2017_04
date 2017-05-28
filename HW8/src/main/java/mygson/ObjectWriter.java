2ed7fe7f36c9fd024e7a7b99d5b78bbb5ab0275a
petrelevich <petrelevich@yandex.ru>
2017-05-29 00:53:53 +0300

    json object writer

--- HW8/src/main/java/mygson/ObjectWriter.java
+++ HW8/src/main/java/mygson/ObjectWriter.java
@@ -1,12 +1,6 @@
 package mygson;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 /**
  * @author sergey
